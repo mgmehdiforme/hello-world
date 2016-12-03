572ca7f2051163fd4a91c26b6c8d40c98dbac54f
Mehdi Golzari <i.entersoft@gmail.com>
2016-12-03 13:48:50 +0330

    متن نمایش دوبار شد

--- githubhelloWorld/githubhelloWorld/Form1.cs
+++ githubhelloWorld/githubhelloWorld/Form1.cs
@@ -19,6 +19,7 @@ namespace githubhelloWorld
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Test");
+            MessageBox.Show("Tدهندنت");
         }
     }
 }
